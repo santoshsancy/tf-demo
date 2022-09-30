0f6408e2ee6f55f12a4c9ab3eb6c98079fbaeff6
Dakarapu <dakaras@shure.com>
2022-09-30 20:05:42 +0530

    updated bucket region

--- terraform.tf
+++ terraform.tf
@@ -2,7 +2,7 @@ terraform {
     backend "s3" {
        bucket  = "tfstate-demo-san"
        key     = "demo/dev/dev.tfstate"
-       region  = "ap-southeast-1" 
+       region  = "ap-south-1" 
       
     }
 }
