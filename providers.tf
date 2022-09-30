91d0a102da055a17dd83644a71629b5442ca5c8a
Dakarapu <dakaras@shure.com>
2022-09-30 20:27:21 +0530

    modified the code

--- providers.tf
+++ providers.tf
@@ -1,6 +1,6 @@
 provider "aws" {
   profile = "default"
   region  = "ap-south-1"
-  # access_key = "${var.access_key}"
-  # secret_key = "${var.secret_key}"
+  access_key = "${var.access_key}"
+  secret_key = "${var.secret_key}"
 }
