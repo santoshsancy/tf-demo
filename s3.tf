b56c1d09b2da8b9e92ec68b7098d773651fe723a
Dakarapu <dakaras@shure.com>
2022-09-30 21:18:51 +0530

    updated the resource

--- s3.tf
+++ s3.tf
@@ -1,5 +1,5 @@
 resource "aws_s3_bucket" "onebucket" {
-   bucket = "testing-s3-with-terraform"
+   bucket = "testing-s3-with-terraform-san"
    acl = "private"
    versioning {
       enabled = true
